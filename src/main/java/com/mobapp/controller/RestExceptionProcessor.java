89581ae33d09e78ba543e4a411ce1285b9cc7dbd
Alex <alex.fruzenshtein@gmail.com>
2013-11-24 13:32:42 +0200

    REST exception handling vol. 2

--- src/main/java/com/mobapp/controller/RestExceptionProcessor.java
+++ src/main/java/com/mobapp/controller/RestExceptionProcessor.java
@@ -2,9 +2,13 @@ package com.mobapp.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -22,11 +26,20 @@ import com.mobapp.exception.helper.FieldErrorDTO;
 @ControllerAdvice
 public class RestExceptionProcessor {
 	
+	@Autowired
+	private MessageSource messageSource;
+	
 	@ExceptionHandler(SmartphoneNotFoundException.class)
 	@ResponseStatus(value=HttpStatus.NOT_FOUND)
 	@ResponseBody
 	public ErrorInfo smartphoneNotFound(HttpServletRequest req, SmartphoneNotFoundException ex) {
-		return new ErrorInfo(req.getRequestURL().toString(), ex.getMessage());
+		Locale locale = LocaleContextHolder.getLocale();
+		String errorMessage = messageSource.getMessage("error.no.smartphone.id", null, locale);
+		
+		errorMessage += ex.getSmartphoneId();
+		String errorURL = req.getRequestURL().toString();
+		
+		return new ErrorInfo(errorURL, errorMessage);
 	}
 	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
--- src/main/java/com/mobapp/exception/SmartphoneNotFoundException.java
+++ src/main/java/com/mobapp/exception/SmartphoneNotFoundException.java
@@ -3,9 +3,14 @@ package com.mobapp.exception;
 public class SmartphoneNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = -2859292084648724403L;
+	private final int smartphoneId;
 	
-	public SmartphoneNotFoundException(int smartphoneId) {
-		super("Smartphone with id: "+smartphoneId+" not found.");
+	public SmartphoneNotFoundException(int id) {
+		smartphoneId = id;
+	}
+	
+	public int getSmartphoneId() {
+		return smartphoneId;
 	}
 
 }
